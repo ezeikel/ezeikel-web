9e7a57a04aff32cf70483681b8424f4f4d939526
Ezeikel Pemberton <hi@ezeikel.dev>
2024-01-21 00:15:44 +0000

    fix: correct import for ImageResponse. Added global error.

---
+++ app/global-error.tsx
@@ -0,0 +1,21 @@
+import * as Sentry from '@sentry/nextjs';
+import NextError from 'next/error';
+import { useEffect } from 'react';
+
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html lang="en">
+      <body>
+        <NextError statusCode={undefined as any} />
+      </body>
+    </html>
+  );
+}
